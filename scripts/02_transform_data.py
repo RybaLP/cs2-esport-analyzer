77a4f0fa36e79b0c8a01ddc400e7aa8923dbf6cc
RybaLP <148885360+RybaLP@users.noreply.github.com>
2026-03-20 21:55:57 +0100

    feat: add team logos to match data

--- scripts/02_transform_data.py
+++ scripts/02_transform_data.py
@@ -32,6 +32,13 @@ def load_raw_file(file_path):
         logger.error(f"The file {file_path} is empty")
     return data
 
+def get_single_url(raw_url):
+                if isinstance(raw_url, list) and raw_url:
+                    return raw_url[0]
+                elif isinstance(raw_url, str):
+                    return raw_url
+                return None
+
 
 def transform_data(data):
     """transform raw match data into a flat, analysis-ready structure"""
@@ -44,15 +51,46 @@ def transform_data(data):
             team1 = teams[0].split(":")[-1].strip() if (len(teams) > 0) else None
             team2 = teams[1].strip() if len(teams) > 1 else None
 
+            # team logos
+            opponents = match.get('opponents',[])
+            t1_logo_raw = opponents[0].get('opponent',{}).get('image_url') if len(opponents) > 0 else None
+            t2_logo_raw = opponents[1].get('opponent',{}).get('image_url') if len(opponents) > 1 else None
+
+            t1_logo = get_single_url(t1_logo_raw)
+            t2_logo = get_single_url(t2_logo_raw)
+
+            # played maps
+            games_list = []
+            for game in match.get('games',[]):
+                if game.get('map'):
+
+                    raw_map_image = game['map'].get('image_url')
+                    safe_map_image = get_single_url(raw_map_image)
+
+                    games_list.append({
+                        "map_name": game['map'].get('name'),
+                        "map_image": safe_map_image,
+                        "score": f"{game.get('results', [{}])[0].get('score', 0)}:{game.get('results', [{}, {}])[1].get('score', 0)}"
+                    })
+
+            overall_res = match.get('results', [])
+            os1 = overall_res[0].get('score', 0) if len(overall_res) > 0 else 0
+            os2 = overall_res[1].get('score', 0) if len(overall_res) > 1 else 0
+
             clean_match = {
-                "id":match.get('id'),
-                "name":match_name,
-                "team1":team1,
-                "team2":team2,
-                "winner":match['winner']['name'],
-                "league":match.get("league",{}).get('name')
+                "id": match.get('id'),
+                "name": match_name,
+                "team1": team1,
+                "team1_logo": t1_logo,
+                "team2": team2,
+                "team2_logo": t2_logo,
+                "winner": match.get('winner', {}).get('name') if match.get('winner') else "Draw",
+                "league": match.get("league", {}).get('name'),
+                "score": f"{os1}:{os2}",
+                "date": match.get('begin_at')
             }
             processed_matches.append(clean_match)
+
     logger.info(f"Matches overall {len(data)}")
     logger.info(f"Finished matches: {len(processed_matches)}")
     return processed_matches
